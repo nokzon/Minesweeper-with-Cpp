bc3443696ca3e8c418eda9088e629c7c066cfce7
Janelle <my.janelle.tan@gmail.com>
2024-04-20 14:04:21 +0800

    Initialise repository.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+#include <ctime>
+#include <vector>
+#include <cstdio>
+using namespace std;
+
+void chooseDifficulty() {
+    cout << "Enter your difficulty level:" << endl;
+    cout << "1: EASY [8 * 8 grid, 10 mines]" << endl;
+    cout << "2: INTERMEDIATE [16 * 16 grid, 40 mines]" << endl;
+
+}
+
+int main() {
+    chooseDifficulty;
+    return 0;
+}

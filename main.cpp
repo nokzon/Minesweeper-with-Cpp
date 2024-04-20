c05ae62b4558cac742bcad7372ae5c7922f3067c
Janelle <my.janelle.tan@gmail.com>
2024-04-20 17:26:18 +0800

    improved level difficulty function

--- main.cpp
+++ main.cpp
@@ -7,12 +7,13 @@ using namespace std;
 
 void chooseLevelDifficulty() {
 
+    enum Difficulty { EASY = 1, INTERMEDIATE = 2, HARD = 3};
+
     int level;
     int side,
         mines;
 
     do {
-
         printf("Enter the difficulty level:\n");
         printf("Press 1: EASY [8 * 8 Cells, 10 Mines]\n");
         printf("Press 2: INTERMEDIATE [16 * 16 Cells, 40 Mines]\n");
@@ -22,15 +23,15 @@ void chooseLevelDifficulty() {
     } while (level != 1 && level != 2 && level != 3);
 
 
-    if (level == 1) {
+    if (level == EASY) {
         side = 9;
         mines = 10;
     } 
-    else if (level == 2) {
+    else if (level == INTERMEDIATE) {
         side = 16;
         mines = 40;
     }
-    else if (level == 3) {
+    else if (level == HARD) {
         side = 24;
         mines = 99;
     } 
